510b1d278b53b7350fa9bc151c229adf6ca45ec3
Christian Osborn <christian.s.osborn@gmail.com>
2020-05-07 19:29:26 -0700

    More sample calls

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-
+//Global Variables//
 var apiKey = "Thva5NLv6RrCnjjzN4yuMRNhH38NosOs";
+
+
+
+//Get List of Type of Events//
+$.ajax({
+    type:"GET",
+    url:"https://app.ticketmaster.com/discovery/v2/classifications.json?apikey=Thva5NLv6RrCnjjzN4yuMRNhH38NosOs",
+    async:true,
+    dataType: "json",
+    success: function(json) {
+                console.log(json);
+                console.log(json.classifications.type.name)
+             },
+    error: function(xhr, status, err) {
+                // This time, we do not end up here!
+             }
+  });
+
+
+
+
 var url = "https://app.ticketmaster.com/discovery/v2/events.json?";
 
-var city = '';
-var zipcode = '';
-
+var city = 'city='+[json.] +'&'
+var zipCode = 'zipcode='[json.]+"&"
+var eventClass = 'classificationName='+[json.]+ "&"
 
 
 $.ajax({
     type:"GET",
-    url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=Thva5NLv6RrCnjjzN4yuMRNhH38NosOs",
+    url:"https://app.ticketmaster.com/discovery/v2/events.json?"+city+zipCodesize+eventClassapikey+apiKey",
     async:true,
     dataType: "json",
     success: function(json) {
@@ -21,4 +42,4 @@ $.ajax({
     error: function(xhr, status, err) {
                 // This time, we do not end up here!
              }
-  });
+  });*/

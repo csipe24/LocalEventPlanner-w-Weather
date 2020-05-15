c4146026aa6a854ac32cd53abff81c99fe9b7c0f
Christian Osborn <christian.s.osborn@gmail.com>
2020-05-14 21:06:21 -0700

    Clean Nav

--- script.js
+++ script.js
@@ -1,62 +1,3 @@
-//<<<<<<< HEAD
-//<<<<<<< HEAD
-//Listner for on Submit
-
-
-console.log("Test");
-$("#target").submit(function(event){
-event.preventDefault();
-console.log("Button Pushed");
-
-})
-
-/*
-console.log("FUCK");
-$("#target").submit(function(event){
-
-    console.log("Yo dawg nice submit");
-    
-//Prevent Default on submit behavior//
-    event.preventDefault();
-
-//Get locally scoped variables//
-    var city = 'city'+$("#cityInput").val() +'&';
-    var zipCode = 'zipcode'+ $("#zipCode").val() +'&';
-    var searchterm = 'searchterm' + $("#searchterm").val() + '&';
-    var startdate  = 'startdate'+ $("startdate").value +'&';
-    var enddate  = 'enddate'+ $("enddate").value +'&';
-
-    //API Query given inputs
-    $.ajax({
-        type:"GET",
-        url:"https://app.ticketmaster.com/discovery/v2/events.json?city=Bellevue&apikey=Thva5NLv6RrCnjjzN4yuMRNhH38NosOs",
-        //url: "https://app.ticketmaster.com/discovery/v2/events.json?"+city+zipCode+searchter+startdate+enddate+"&apikey=Thva5NLv6RrCnjjzN4yuMRNhH38NosOs";
-        async:true,
-        dataType: "json",
-        success: function(json) {
-                    console.log(json);
-                    console.log(json._embedded);
-                    const array1 = json._embedded.events;
-                    array1.forEach(element => 
-                    console.log(element.name)
-                    //console.log(element.image);
-                    ) 
-                },
-        error: function(xhr, status, err) {
-                    // This time, we do not end up here!
-                }
-    });
-
-})
-//<<<<<<< HEAD
-
-//Title 
-//Description
-//Image*/
-//=======
-//=======
-//=======
-//>>>>>>> master
 // Create current date JS
 var dateEl = $("#date");
 $(dateEl).text(moment(new Date()).format("dddd, MMM Do YYYY, h:mm a"));
@@ -93,12 +34,10 @@ function getWeather(weatherCity){
         console.log(response);
         for(i=0; i<5; i++){
           var wDiv= $("#weather-div");
-          var cityTemp = ("Temp: "+(Math.floor(response.list[i].main.temp - 273.15)*9/5+32)+"°F");
-          var cityHumidity = response.list[i].main.humidity;
-          var cityWindSpeed = response.list[i].wind.speed;
-          $(wDiv).append(cityTemp+"°F");
-          $(wDiv).append("Humidity: "+cityHumidity+"%");
-          $(wDiv).append("Wind Spd: "+cityWindSpeed+"MPH");
+          var cityTemp = $("<span></span><br>").text("Temp: "+(Math.floor(response.list[i].main.temp - 273.15)*9/5+32)+"°F");
+          var cityHumidity = $("<span></span><br>").text("Humidity: "+ response.list[i].main.humidity);
+          var cityWindSpeed = $("<span></span><br>").text("Wind Speed: "+response.list[i].wind.speed);
+          $(wDiv).append(cityTemp,cityHumidity);
         };
 
       });
@@ -148,8 +87,4 @@ $.ajax({
 
       
 
-//<<<<<<< HEAD
-//>>>>>>> 47c8dd04f5e47cca902d0320f1c7d3958f7d2d2b
-//>>>>>>> master
-//=======
-//>>>>>>> master
+
